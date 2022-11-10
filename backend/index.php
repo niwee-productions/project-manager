b7d653897e783d1fe90d78d66ab587e2466253bf
BruForm <bpayet.gsm@free.fr>
2022-11-10 14:52:54 +0100

    [DEV] first step with API

--- backend/index.php
+++ backend/index.php
@@ -1,3 +1,33 @@
 <?php
-echo "test";
-?>
+
+namespace Niwee\ProjectManager;
+
+// Require composer autoloader
+require __DIR__ . '/vendor/autoload.php';
+
+use Bramus\Router\Router;
+
+class Index
+{
+    /**
+     * @var Router
+     */
+    private $router;
+
+    public function __construct()
+    {
+        // Create Router instance
+        $this->router = new Router();
+
+        $this->router->get('/api/test', function () {
+//            echo "Coucou";
+            $text = ["text" => "coucou depuis l'api"];
+            echo json_encode($text, JSON_UNESCAPED_UNICODE);
+        });
+
+        $this->router->run();
+    }
+
+}
+
+$index = new Index();
