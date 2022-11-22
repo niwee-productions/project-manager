cd99139e51ca0e3bf483b7e220b60ac6d4789bbc
BruForm <bpayet.gsm@free.fr>
2022-11-22 10:56:40 +0100

    [DEV] frontend : replaced with REACT generated templates

---
+++ frontend/src/Pages/Client.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+export default class Client extends React.Component {
+  constructor(props) {
+    super(props)
+    this.props = props
+  }
+
+  componentDidMount() {
+    document.title = 'Title'
+  }
+
+  render() {
+    return <p>Here you are</p>
+  }
+}
